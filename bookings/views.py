5d178b3949cf1491df9bcfe4ba5f2deba4a4fd9b
Anshuman Mishra <120048120+Ansh9454@users.noreply.github.com>
2026-03-03 22:47:41 +0530

    Implement get_queryset for role-based booking filtering
    Add get_queryset method to filter bookings based on user role.

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+    def get_queryset(self):
+        user = self.request.user
+
+        queryset = Booking.objects.select_related(
+            "vehicle",
+            "service",
+            "partner"
+        )
+
+        if user.role == "CUSTOMER":
+            return queryset.filter(customer=user)
+
+        if user.role == "PARTNER":
+            return queryset.filter(partner=user)
+
+        return Booking.objects.none()
