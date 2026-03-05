140506f7130ed4d1cabc6d37c113781cb73a887a
Anshuman Mishra <120048120+Ansh9454@users.noreply.github.com>
2026-03-05 21:39:16 +0530

    Filter queryset by status parameter in views

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+        status_param = self.request.query_params.get("status")
+        if status_param:
+            queryset = queryset.filter(status=status_param)
